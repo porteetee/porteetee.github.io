8a5c14871bc1cc712779a89c570255bee97b751d
porteetee <por123ja@gmail.com>
2025-06-11 16:08:35 +0700

    cat rotate

--- js/img-rotate.js
+++ js/img-rotate.js
@@ -1,34 +1,37 @@
 const cat = document.getElementById('my-cat');
 let rotation = 0;
-let startX ;
-let lastX = 0;
+let lastX=0 ,lastY=0;
 let velocity = 0;
 let isDragging = false;
 let inertiaAnimation;
 
 cat.addEventListener('click', (e) => {
+    cat.style.animation = "none";
     cancelAnimationFrame(inertiaAnimation);
     velocity +=  1000;
     applyInertia();
 });
 
 cat.addEventListener('touchstart', (e) => {
+    cat.style.animation = "none";
     cancelAnimationFrame(inertiaAnimation);
     isDragging = true;
-    startX = lastX = e.touches[0].clientX;
+    lastX = e.touches[0].clientX;
 });
 
 cat.addEventListener('touchmove', (e) => {
     if (!isDragging) return;
 
     const currentX = e.touches[0].clientX;
+    const currentY = e.touches[0].clientY;
     const diffX = currentX - lastX;
-
-    velocity = diffX;
-    rotation += diffX;
+    const diffY = currentY - lastY;
+    velocity = diffX+ diffY;
+    rotation += diffX+ diffY;
     cat.style.transform = `rotate(${rotation}deg)`;
 
     lastX = currentX;
+    lastY = currentY;
     e.preventDefault();
 });
 
@@ -39,9 +42,17 @@ cat.addEventListener("touchend", () => {
 
 function applyInertia() {
     velocity *= 0.95;
-    if (Math.abs(velocity) > 0.1) {
+    if (Math.abs(velocity) > 2) {
         rotation += velocity;
         cat.style.transform = `rotate(${rotation}deg)`;
         inertiaAnimation = requestAnimationFrame(applyInertia);
+    }else {
+        velocity = 2;
+        spinForever();
     }
+}
+function spinForever() {
+    rotation += velocity;
+    cat.style.transform = `rotate(${rotation}deg)`;
+    inertiaAnimation = requestAnimationFrame(spinForever);
 }
